a1ad180abbd50c30bf57748b83ea17f391aad773
chiri <chirizxc@proton.me>
2026-04-30 13:02:02 +0300

    update

--- src/uuid/uuid_obj.rs
+++ src/uuid/uuid_obj.rs
@@ -1,13 +1,15 @@
+use pyo3::ffi::PyObject;
+
 #[repr(C)]
 pub struct UUIDObject {
-    pub ob_base: pyo3::ffi::PyObject,
+    pub ob_base: PyObject,
     pub hi: u64,
     pub lo: u64,
 }
 
 impl UUIDObject {
     #[inline]
-    pub fn from_self<'a>(ptr: *mut pyo3::ffi::PyObject) -> &'a Self {
+    pub fn from_self<'a>(ptr: *mut PyObject) -> &'a Self {
         debug_assert!(!ptr.is_null());
         unsafe { &*ptr.cast::<Self>() }
     }
