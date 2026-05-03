405ceb73b3b232181b1d512bd709ba022ec687b6
chiri <chirizxc@proton.me>
2026-05-03 23:59:06 +0300

    try fix

--- build.rs
+++ build.rs
@@ -1,3 +1,5 @@
+use pyo3_build_config::{BuildFlag, PythonImplementation};
+
 fn main() {
     pyo3_build_config::use_pyo3_cfgs();
 
@@ -5,7 +7,19 @@ fn main() {
         return;
     }
 
-    if let Some(lib_name) = pyo3_build_config::get().lib_name.as_deref() {
+    let config = pyo3_build_config::get();
+    if let Some(lib_name) = config.lib_name.as_ref() {
         println!("cargo:rustc-cfg=pyo3_dll=\"{lib_name}\"");
+    } else if config.implementation == PythonImplementation::CPython {
+        let gil = if config.is_free_threaded() { "t" } else { "" };
+        let debug = if config.build_flags.0.contains(&BuildFlag::Py_DEBUG) {
+            "_d"
+        } else {
+            ""
+        };
+        println!(
+            "cargo:rustc-cfg=pyo3_dll=\"python{}{}{gil}{debug}\"",
+            config.version.major, config.version.minor
+        );
     }
 }
