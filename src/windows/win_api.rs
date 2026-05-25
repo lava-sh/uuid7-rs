fd70bc357f0c2eeab673fee9dc6e8a240971e5c9
chiri <chirizxc@proton.me>
2026-05-25 13:54:43 +0300

    upd

--- src/windows/win_api.rs
+++ src/windows/win_api.rs
@@ -7,12 +7,13 @@ pub struct FILETIME {
     pub dwHighDateTime: u32,
 }
 
-#[link(name = "kernel32")]
-unsafe extern "system" {
-    pub fn GetSystemTimePreciseAsFileTime(lpSystemTimeAsFileTime: *mut FILETIME);
-}
-
-#[link(name = "mincore")]
-unsafe extern "system" {
-    pub fn QueryInterruptTime(lpInterruptTime: *mut u64);
-}
+windows_link::link!(
+    "mincore"
+    "system"
+    fn QueryInterruptTime(lpInterruptTime: *mut u64)
+);
+windows_link::link!(
+    "kernel32"
+    "system"
+    fn GetSystemTimePreciseAsFileTime(lpSystemTimeAsFileTime: *mut FILETIME)
+);
