f5d3b35e2e062cb5695341ce829512f0f6b057e8
chiri <chirizxc@proton.me>
2026-06-09 19:52:42 +0300

    update

--- src/python/py_3_13.rs
+++ src/python/py_3_13.rs
@@ -1,11 +1,12 @@
-use std::ffi::c_void;
-
-use pyo3::ffi::{
-    Py_ASNATIVEBYTES_BIG_ENDIAN, Py_ASNATIVEBYTES_UNSIGNED_BUFFER, PyLong_FromUnsignedNativeBytes,
-    PyObject,
-};
+use pyo3::ffi::{Py_ASNATIVEBYTES_UNSIGNED_BUFFER, PyLong_FromUnsignedNativeBytes, PyObject};
 
 use crate::parse::uuid_to_bytes;
+
+#[cfg(target_endian = "big")]
+const ENDIAN_FLAG: i32 = pyo3::ffi::Py_ASNATIVEBYTES_BIG_ENDIAN;
+
+#[cfg(target_endian = "little")]
+const ENDIAN_FLAG: i32 = pyo3::ffi::Py_ASNATIVEBYTES_LITTLE_ENDIAN;
 
 pub fn uuid_int_from_parts(hi: u64, lo: u64) -> *mut PyObject {
     let mut buf = [0_u8; 16];
@@ -13,9 +14,9 @@ pub fn uuid_int_from_parts(hi: u64, lo: u64) -> *mut PyObject {
     unsafe {
         uuid_to_bytes(hi, lo, buf.as_mut_ptr());
         PyLong_FromUnsignedNativeBytes(
-            buf.as_ptr().cast::<c_void>(),
-            16,
-            Py_ASNATIVEBYTES_BIG_ENDIAN | Py_ASNATIVEBYTES_UNSIGNED_BUFFER,
+            buf.as_ptr().cast(),
+            buf.len(),
+            ENDIAN_FLAG | Py_ASNATIVEBYTES_UNSIGNED_BUFFER,
         )
     }
 }
