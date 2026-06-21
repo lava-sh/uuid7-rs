8362334895cef319edacdd40a3a2d87f70886e86
chiri <chirizxc@proton.me>
2026-06-21 20:30:20 +0300

    Simplify build.rs by removing OS check
    Removed conditional logic for non-Windows OS in build script.

--- build.rs
+++ build.rs
@@ -1,22 +1,3 @@
-use pyo3_build_config::PythonImplementation;
-
 fn main() {
     pyo3_build_config::use_pyo3_cfgs();
-
-    if std::env::var("CARGO_CFG_TARGET_OS").as_deref() != Ok("windows") {
-        return;
-    }
-
-    let config = pyo3_build_config::get();
-
-    if let Some(lib_name) = config.lib_name().as_ref() {
-        println!("cargo:rustc-cfg=pyo3_dll=\"{lib_name}\"");
-    } else if config.implementation() == PythonImplementation::CPython && !config.is_free_threaded()
-    {
-        println!(
-            "cargo:rustc-cfg=pyo3_dll=\"python{}{}\"",
-            config.version().major,
-            config.version().minor
-        );
-    }
 }
