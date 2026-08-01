624199d5fb3036d93acfe3c3536056cb7df9abb6
chiri <chirizxc@proton.me>
2026-08-01 15:49:10 +0300

    try fix

--- src/parse.rs
+++ src/parse.rs
@@ -255,22 +255,21 @@ pub fn parse_uuid_fields(value: *mut PyObject, hi: &mut u64, lo: &mut u64) -> c_
     let mut parts = [0_u64; 6];
 
     for i in 0_usize..6 {
-        let item =
-            cfg_select! {
-                not(PyPy) => unsafe {
-                    pyo3::ffi::PySequence_Fast_GET_ITEM(seq, i.cast_signed())
-                },
-                PyPy => ({
-                    use pyo3::ffi::PySequence_GetItem;
+        let item = cfg_select! {
+            not(PyPy) => unsafe {
+                pyo3::ffi::PySequence_Fast_GET_ITEM(seq, i.cast_signed())
+            },
+            PyPy => ({
+                use pyo3::ffi::PySequence_GetItem;
 
-                    let item = unsafe { PySequence_GetItem(seq, i.cast_signed()) };
-                    if item.is_null() {
-                        unsafe { Py_DECREF(seq) };
-                        return -1;
-                    }
-                    item
-                }),
-            };
+                let item = unsafe { PySequence_GetItem(seq, i.cast_signed()) };
+                if item.is_null() {
+                    unsafe { Py_DECREF(seq) };
+                    return -1;
+                }
+                item
+            }),
+        };
 
         let v = unsafe { PyLong_AsUnsignedLongLong(item) };
         unsafe {
