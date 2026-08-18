89596f39080a935e0c64ad4f113707759a58f09a
chiri <chirizxc@proton.me>
2026-08-18 20:02:35 +0300

    update `now_ms`

--- src/unix/time.rs
+++ src/unix/time.rs
@@ -2,11 +2,10 @@ use std::time::{SystemTime, UNIX_EPOCH};
 
 #[inline]
 pub fn now_ms() -> u64 {
-    let duration = SystemTime::now()
+    SystemTime::now()
         .duration_since(UNIX_EPOCH)
-        .unwrap_or_default();
-
-    duration.as_secs() * 1000 + u64::from(duration.subsec_millis())
+        .expect("system clock is before UNIX_EPOCH")
+        .as_millis() as u64
 }
 
 #[inline]
